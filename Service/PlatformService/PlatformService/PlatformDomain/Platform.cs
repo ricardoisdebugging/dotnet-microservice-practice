5523d80085a622dc9c264d16d101bb472797adbb
Haitao <ricardo-haitao.mao@cn.abb.com>
2022-04-29 23:03:44 +0800

    create model

---
+++ Service/PlatformService/PlatformService/PlatformDomain/Platform.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PlatformService.PlatformDomain
+{
+    public class Platform
+    {
+        [Key]
+        [Required]
+        public int PlatformId { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Publisher { get; set; }
+
+        [Required]
+        public string Cost { get; set; }
+    }
+}
