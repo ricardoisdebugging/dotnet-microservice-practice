a8e29834f2771cc4f91e4aec809d3004e0859fa1
Haitao <ricardo-haitao.mao@cn.abb.com>
2022-04-29 23:04:20 +0800

    create application db context

---
+++ Service/PlatformService/PlatformService/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PlatformService.PlatformDomain;
+
+namespace PlatformService.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> opt)
+            :base(opt)
+        {
+
+        }
+
+        public DbSet<Platform> Platforms { get; set; }
+    }
+}
