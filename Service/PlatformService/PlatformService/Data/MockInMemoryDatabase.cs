555af6c20838e970ea2fb9b95c7546d5ed8b1b25
Haitao <ricardo-haitao.mao@cn.abb.com>
2022-04-29 23:05:34 +0800

    configure mock data and in memory database

---
+++ Service/PlatformService/PlatformService/Data/MockInMemoryDatabase.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using PlatformService.PlatformDomain;
+using System;
+using System.Linq;
+
+namespace PlatformService.Data
+{
+    public class MockInMemoryDatabase
+    {
+        public static void MockPopulation(IApplicationBuilder app)
+        {
+            using (var serviceScope = app.ApplicationServices.CreateScope())
+            {
+                SeedData(serviceScope.ServiceProvider.GetService<ApplicationDbContext>());
+            }
+        }
+
+private static void SeedData(ApplicationDbContext context)
+{
+    if(!context.Platforms.Any())
+    {
+        Console.WriteLine(">>>Seeding data...");
+
+        context.Platforms.AddRange(
+            new Platform() { Name = "Dot Net", Publisher = "Microsoft", Cost="Free"},
+            new Platform() { Name = "SQL Server Express", Publisher = "Microsoft", Cost = "Free" },
+            new Platform() { Name = "Kubernetes", Publisher = "Cloud Native Computing Foundation", Cost = "Free" }
+        );
+
+        context.SaveChanges();
+    }
+    else
+    {
+        Console.WriteLine(">>>Seed data exist...");
+    }
+}
+    }
+}
