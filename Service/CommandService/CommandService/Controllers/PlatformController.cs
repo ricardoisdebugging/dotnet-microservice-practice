3d9a8809bb0729712b5d632eb8337f72e69c504f
RICARDOMINIPC\Ricardo <haitaomao0730@foxmail.com>
2022-06-28 08:51:47 +0800

    implement command service

---
+++ Service/CommandService/CommandService/Controllers/PlatformController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace CommandService.Controllers
+{
+    [Route("api/c/[controller]s")]
+    [ApiController]
+    public class PlatformController : ControllerBase
+    {
+
+        [HttpPost]
+        public ActionResult TestInboudConnection()
+        {
+            string feedback = ">>>Inbound Post Command Service";
+            Console.WriteLine($"{feedback}");
+
+            return Ok(feedback);
+        }
+    }
+}
